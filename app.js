56176c45788f6a80571b0c1101d66ef60f37bac4
Mthobisi Mkhonto <mmkhonto3@gmail.com>
2017-03-27 08:13:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var builder = require('botbuilder');
 // Bot Setup
 //=========================================================
 
-// Setup Restify Server
+/*// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
@@ -38,6 +38,43 @@ bot.dialog('How old are you', function (session) {
 //LUIS Model
 var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?xxxxxxx';
 var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer]});
+var dialog = new builder.IntentDialog({ recognizers: [recognizer]});*/
 
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'f7d843c6-24e4-448d-8f62-70d910b29b6f',
+    appPassword: 'OOmVxg1kNob7O72jgPCdjdi'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('Hi', function (session) {
+    session.send("Greetings");
+});
+bot.dialog('How are you', function (session) {
+    session.send("Fine thanks");
+});
+bot.dialog('What is your name', function (session) {
+    session.send("Computer");
+});
+bot.dialog('How old are you', function (session) {
+    session.send("1 week");
+});
 
