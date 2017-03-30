cf09dd55ff96b76515bdf522ec9e047914eb762e
Mthobisi Mkhonto <mmkhonto3@gmail.com>
2017-03-30 08:27:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,30 +22,30 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 // Bots Dialogs
 //=========================================================
-var HelpMessage = '\n * If you want to know which programming I\'m using for my searches type \'current programming\'. \n * Want to change the current programming? Type \'change programming to programmingName\'. \n * Want to change it just for your searches? Type \'change my programming to programmingName\'';
-var UserNameKey = 'UserName';
-var UserWelcomedKey = 'UserWelcomed';
-var ProgrammingKey = 'Programming';
+var Hmsg = '\n * If you want to know which city I\'m using for my searches type \'current city\'. \n * Want to change the current city? Type \'change city to cityName\'. \n * Want to change it just for your searches? Type \'change my city to cityName\'';
+var UserKey = 'UserName';
+var UserWelKey = 'UserWelcomed';
+var TownKey = 'City';
 
-// Setup bot with default dialog
+// Default dialog
 var bot = new builder.UniversalBot(connector, function (session) {
 
-    // initialize with default Programming
-    if (!session.conversationData[ProgrammingKey]) {
-        session.conversationData[ProgrammingKey] = 'Java';
-        session.send('Welcome to the Search Programming bot. I\'m currently configured to search for things in %s', session.conversationData[ProgrammingKey]);
+    // initialize with default city
+    if (!session.conversationData[TownKey]) {
+        session.conversationData[TownKey] = 'Gauteng';
+        session.send('Welcome to the Search City bot. I\'m currently configured to search for things in %s', session.conversationData[TownKey]);
     }
 
     // is user's name set? 
-    var userName = session.userData[UserNameKey];
+    var userName = session.userData[UserKey];
     if (!userName) {
         return session.beginDialog('greet');
     }
 
     // has the user been welcomed to the conversation?
-    if (!session.privateConversationData[UserWelcomedKey]) {
-        session.privateConversationData[UserWelcomedKey] = true;
-        return session.send('Welcome back %s! Remember the rules: %s', userName, HelpMessage);
+    if (!session.privateConversationData[UserWelKey]) {
+        session.privateConversationData[UserWelKey] = true;
+        return session.send('Welcome back %s! Remember the rules: %s', userName, Hmsg);
     }
 
     session.beginDialog('search');
@@ -57,64 +57,64 @@ bot.set('persistConversationData', true);
 // search dialog
 bot.dialog('search', function (session, args, next) {
     // perform search
-    var program = session.privateConversationData[ProgrammingKey] || session.conversationData[ProgrammingKey];
-    var userName = session.userData[UserNameKey];
-    var messageText = session.message.text.trim();
-    session.send('%s, wait a few seconds. Searching for \'%s\' in \'%s\'...', userName, messageText, program);
-    session.send('https://www.bing.com/search?q=%s', encodeURIComponent(messageText + ' in ' + program));
+    var city = session.privateConversationData[TownKey] || session.conversationData[TownKey];
+    var userName = session.userData[UserKey];
+    var msgT = session.message.text.trim();
+    session.send('%s, wait a few seconds. Searching for \'%s\' in \'%s\'...', userName, msgT, city);
+    session.send('https://www.bing.com/search?q=%s', encodeURIComponent(msgT + ' in ' + city));
     session.endDialog();
 });
 
 // reset bot dialog
 bot.dialog('reset', function (session) {
     // reset data
-    delete session.userData[UserNameKey];
-    delete session.conversationData[ProgrammingKey];
-    delete session.privateConversationData[ProgrammingKey];
-    delete session.privateConversationData[UserWelcomedKey];
+    delete session.userData[UserKey];
+    delete session.conversationData[TownKey];
+    delete session.privateConversationData[TownKey];
+    delete session.privateConversationData[UserWelKey];
     session.endDialog('Ups... I\'m suffering from a memory loss...');
 }).triggerAction({ matches: /^reset/i });
 
-// print current programming dialog
-bot.dialog('printCurrentProgramming', function (session) {
-    var userName = session.userData[UserNameKey];
-    var defaultProgram = session.conversationData[ProgrammingKey];
-    var userProgram = session.privateConversationData[ProgrammingKey];
-    if (!defaultProgram) {
+// print current city dialog
+bot.dialog('printCurrentCity', function (session) {
+    var userName = session.userData[UserKey];
+    var defaultCity = session.conversationData[TownKey];
+    var userCity = session.privateConversationData[TownKey];
+    if (!defaultCity) {
         session.endDialog('I don\'t have a search city configured yet.');
-    } else if (userProgram) {
+    } else if (userCity) {
         session.endDialog(
-            '%s, you have overridden the programming. Your searches are for things in %s. The default conversation programming is %s.',
-            userName, userProgram, defaultProgram);
+            '%s, you have overridden the city. Your searches are for things in %s. The default conversation city is %s.',
+            userName, userCity, defaultCity);
     } else {
-        session.endDialog('Hey %s, I\'m currently configured to search for things in %s.', userName, defaultProgram);
+        session.endDialog('Hey %s, I\'m currently configured to search for things in %s.', userName, defaultCity);
     }
-}).triggerAction({ matches: /^current programming/i });
+}).triggerAction({ matches: /^current city/i });
 
-// change current programming dialog
-bot.dialog('changeCurrentProgramming', function (session, args) {
-    // change default programming
-    var newProgram = args.intent.matched[1].trim();
-    session.conversationData[ProgrammingKey] = newProgram;
-    var userName = session.userData[UserNameKey];
-    session.endDialog('All set %s. From now on, all my searches will be for things in %s.', userName, newProgram);
-}).triggerAction({ matches: /^change programming to (.*)/i });
+// change current city dialog
+bot.dialog('changeCurrentCity', function (session, args) {
+    // change default city
+    var newCity = args.intent.matched[1].trim();
+    session.conversationData[TownKey] = newCity;
+    var userName = session.userData[UserKey];
+    session.endDialog('All set %s. From now on, all my searches will be for things in %s.', userName, newCity);
+}).triggerAction({ matches: /^change city to (.*)/i });
 
-// change my current programming dialog
-bot.dialog('changeMyCurrentProgramming', function (session, args) {
-    // change user's programming
-    var newProgram = args.intent.matched[1].trim();
-    session.privateConversationData[ProgrammingKey] = newProgram;
-    var userName = session.userData[UserNameKey];
-    session.endDialog('All set %s. I have overridden the programming to %s just for you', userName, newProgram);
-}).triggerAction({ matches: /^change my programming to (.*)/i });
+// change my current city dialog
+bot.dialog('changeMyCurrentCity', function (session, args) {
+    // change user's city
+    var newCity = args.intent.matched[1].trim();
+    session.privateConversationData[TownKey] = newCity;
+    var userName = session.userData[UserKey];
+    session.endDialog('All set %s. I have overridden the city to %s just for you', userName, newCity);
+}).triggerAction({ matches: /^change my city to (.*)/i });
 
 // Greet dialog
 bot.dialog('greet', new builder.SimpleDialog(function (session, results) {
     if (results && results.response) {
-        session.userData[UserNameKey] = results.response;
-        session.privateConversationData[UserWelcomedKey] = true;
-        return session.endDialog('Welcome %s! %s', results.response, HelpMessage);
+        session.userData[UserKey] = results.response;
+        session.privateConversationData[UserWelKey] = true;
+        return session.endDialog('Welcome %s! %s', results.response, Hmsg);
     }
 
     builder.Prompts.text(session, 'Before get started, please tell me your name?');
